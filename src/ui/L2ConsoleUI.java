ed47e1f9d76a5feef0f4187c60e3c197a71d7bbd
lucaslopez25 <lucasviniciuslopez@gmail.com>
2019-10-06 22:08:07 -0300

    Console com funções básicas completo

--- src/ui/L2ConsoleUI.java
+++ src/ui/L2ConsoleUI.java
@@ -1,8 +1,11 @@
 package ui;
 
 import java.io.BufferedReader;
+import session.RegistroCompra;
 import java.io.IOException;
 import java.io.InputStreamReader;
+
+import exceptions.PigmentoException;
 
 //import //session
 //import //exceptions na session
@@ -10,24 +13,87 @@ import java.io.InputStreamReader;
 public class L2ConsoleUI {
 	
 	private BufferedReader br;
+	private RegistroCompra rc;
 	
 	public L2ConsoleUI () throws ClassNotFoundException, IOException {
 		this.br = new BufferedReader(new InputStreamReader (System.in));
-		
+		this.rc = new RegistroCompra ();
 	}
 	
 	
+	private void verPigmentos () throws IOException, NumberFormatException, PigmentoException {
+				
+		
+		//------------------
+		
+		System.out.println("Menu");
+		this.printLinha();
+		
+	}
+	
+	private void debitarPigmento () throws IOException, NumberFormatException, PigmentoException {
+		String corHexa;
+		String result;
+		double quantidade;
+		char opcao;
+		
+		//------------------
+		
+		System.out.println("Fazer Compra");
+		this.printLinha();
+		System.out.println("Informe a cor desejada em código Hexadecimal:");
+		corHexa = this.br.readLine();
+		System.out.println("Informe a quantidade de litros a debitar:");
+		quantidade = Double.parseDouble(this.br.readLine());
+		result = this.rc.buscarCor(corHexa, quantidade);
+		System.out.println("A cor mais próxima encontrada foi:  " + result);
+		System.out.println("Deseja prosseguir? s ou n?");
+		opcao = (char)System.in.read();
+		if ((opcao == 'S') || (opcao == 's')) {
+			try {
+				this.rc.registrarCompra(result, quantidade);
+			}
+			catch (PigmentoException pex){
+				System.err.println("Erro - Pigmento:  " + pex);
+			}
+		}
+		else {
+			System.out.println("Retornando para o menu... \n \n \n");
+		}
+	}
+	
+	private void debitarPigmentoRelampago () throws IOException, NumberFormatException, PigmentoException {
+		String corHexa;
+		double quantidade;
+		
+		//------------------
+		try {
+			System.out.println("Informe a cor desejada em código Hexadecimal:");
+			corHexa = this.br.readLine();
+			System.out.println("Informe a quantidade de litros a debitar:");
+			quantidade = Double.parseDouble(this.br.readLine());
+			this.rc.registrarCompra(this.rc.buscarCor(corHexa, quantidade), quantidade);
+		}
+		catch (PigmentoException pex) {
+			System.err.println("Erro - Pigmento:  " + pex);
+		}
+	}
+	
+	private void printLinha () {
+		System.out.println("------------------------");
+	}
 	
 	public void run() {
 		char opc = '0';
 		do {
 			
 			System.out.println("Menu");
-			System.out.println("------------------------");
+			this.printLinha();
 			System.out.println("Digite 0 para SAIR");
 			System.out.println("Digite 1 para ver pigmentos disponiveis + quantidade de tinta");
 			System.out.println("Digite 2 para debitar um pigmento");
-			System.out.println("------------------------");
+			System.out.println("Digite 3 para compra-relâmpago");
+			this.printLinha();
 			
 			try {
 				opc = br.readLine().charAt(0);
@@ -38,16 +104,37 @@ public class L2ConsoleUI {
 			switch (opc) {
 				case '1' :
 					try {
-						
+						this.verPigmentos();
 					}
-					catch {
-						
+					catch (NumberFormatException nfe){
+						System.err.println("Valor inválido");
 					}
-					catch {
-						
+					catch (IOException | PigmentoException ioe){
+						System.err.println("Operação não concluída:" + ioe);
 					}
 				break;
 				case '2' :
+					try {
+						this.debitarPigmento();
+					}
+					catch (NumberFormatException nfe){
+						System.err.println("Valor inválido");
+					}
+					catch (IOException | PigmentoException ioe){
+						System.err.println("Operação não concluída:" + ioe);
+					}
+				break;
+				case '3' :
+					try {
+						this.debitarPigmentoRelampago();
+					}
+					catch (NumberFormatException nfe){
+						System.err.println("Valor inválido");
+					}
+					catch (IOException | PigmentoException ioe){
+						System.err.println("Operação não concluída:" + ioe);
+					}
+					
 			}
 			
 		} while (opc != '0');
